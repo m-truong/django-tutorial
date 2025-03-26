b08efa87905ed5d1b543ad5e98a8ae1e2e82bd52
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-26 19:04:09 -0400

    Update notes.py with additional explanations about Django projects and apps

--- notes.py
+++ notes.py
@@ -1,3 +1,7 @@
 # VS Code activates the environment automatically when you use the shortcut to activeate the virtual python environment Terminal: Create New Terminal (⌃⇧`).
 
 # In Django terminology, a "Django project" is composed of several site-level configuration files, along with one or more "apps" that you deploy to a web host to create a full web application.
+
+#  A Django project can contain multiple apps, each of which typically has an independent function in the project, and the same app can be in multiple Django projects.
+
+# An app, for its part, is just a Python package that follows certain conventions that Django expects.
