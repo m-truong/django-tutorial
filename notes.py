4908875c00443d7028ff55e1a477aa0e82e4833b
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-26 19:00:07 -0400

    Initial commit

---
+++ notes.py
@@ -0,0 +1,3 @@
+# VS Code activates the environment automatically when you use the shortcut to activeate the virtual python environment Terminal: Create New Terminal (⌃⇧`).
+
+# In Django terminology, a "Django project" is composed of several site-level configuration files, along with one or more "apps" that you deploy to a web host to create a full web application.
