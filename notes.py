a2d98d440b0050a9f1bb1d50f5339a8cec494aca
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-26 21:15:09 -0400

    Add guidance on naming conventions for Django projects and apps in notes.py

--- notes.py
+++ notes.py
@@ -5,3 +5,5 @@
 #  A Django project can contain multiple apps, each of which typically has an independent function in the project, and the same app can be in multiple Django projects.
 
 # An app, for its part, is just a Python package that follows certain conventions that Django expects.
+
+# You’ll need to avoid naming projects after built-in Python or Django components. In particular, this means you should avoid using names like django (which will conflict with Django itself) or test (which conflicts with a built-in Python package).
