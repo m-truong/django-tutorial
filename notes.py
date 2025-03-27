e377b149786a6ee08b0b9ebb236bc15a6357f4be
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-27 13:18:26 -0400

    Update notes.py with development server behavior and add WSGI compiled file

--- notes.py
+++ notes.py
@@ -22,4 +22,4 @@
 
 # Note: production-level data store such as PostgreSQL, MySQL, and SQL Server.
 
-#
+# The development server automatically reloads Python code for each request as needed. You don’t need to restart the server for code changes to take effect. However, some actions like adding files don’t trigger a restart, so you’ll have to restart the server in these cases.
