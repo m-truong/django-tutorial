130254102368fb62d4c2bd61104d271c9de08f29
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-27 13:28:30 -0400

    Add comment to views.py explaining file structure and usage

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# note: the command "startapp" will generate a  number of code files and one subfolder.
+
+# Of these, you frequently work with views.py (that contains the functions that define pages in your web app) and models.py (that contains classes defining your data objects).
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
